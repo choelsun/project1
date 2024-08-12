b6ea4dd723085d42e4fb9d4b439148de2928a02d
kim-chiel-sum <140506s@gmail.com>
2024-08-12 16:15:22 +0900

    요청 처리 및 json 응답

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+import { fileURLToPath } from "url";
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+
 import express from "express";
 import mariadb from "mariadb";
 import dotenv from "dotenv";
@@ -6,35 +10,32 @@ dotenv.config();
 
 const app = express()
 
+app.use(express.json()); // json 포맷으로 인식
+
 // route : .get(): 받기, .post(): 보내기, .put(): 부분 수정 .delete(): 삭제
 // RESTful API : REpresentatinal (대표성 있는 방식으로 요청 URL을 생성하는 규칙)
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  // console.log(__dirname);
+  res.sendFile(__dirname+"public/index.html")
+  // res.send('<h1>Hello World</h1>')
 })
 // db connection
 const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
   connectionLimit: 5
 });
 app.get('/getAllUsers', function (req, res) {
   // MariaDB 연결 드라이버를 통해 서버의 DBMS 데이터로 접근 할 수 있어야 함
-
-
   pool.getConnection()
   .then(conn => {
-  
-    conn.query("SELECT 1 as val")
+    console.log("============== MairaDB is connectde! ===============")
+    conn.query("SELECT * FROM users")
       .then((rows) => {
-        console.log(rows); //[ {val: 1}, meta: ... ]
-        //Table must have been created before 
-        // " CREATE TABLE myTable (id int, val varchar(255)) "
-        return rows;
-      })
-      .then((res) => {
-        console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-        conn.end();
+         res.status(200).json(rows);    // res 응답 객체에서 제공하는 .json() 메소드로 데이터를 전송
+         return conn.end(); // 또 다른 요청에 응답하기 위해 한 번 요청 처리하면 접속 끊기
       })
       .catch(err => {
         //handle error
@@ -43,7 +44,9 @@ app.get('/getAllUsers', function (req, res) {
       })
       
   }).catch(err => {
+    console.log(err); // DB연결 시 에러가 발생되면 에러 출력
     //not connected
   });
+})
 
 app.listen(3000)
